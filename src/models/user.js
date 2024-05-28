4a7674f2f6565cf28814baa34430463d8a0a544c
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-05-29 01:36:45 +0530

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -1,16 +1,38 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const passportLocalMongoose = require('passport-local-mongoose');
 const findOrCreate = require('mongoose-findorcreate');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true },
-    email: { type: String, unique: true, required: true },
+    username: { type: String, required: true },
+    email: String,
+    password: String,
     googleId: String,
     facebookId: String,
-    auth0Id: String
+    auth0Id: String,
+    isEmployee: { type: Boolean, default: false }
 });
+
+// Password hashing middleware
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+// Password comparison method
+userSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+module.exports = User;
