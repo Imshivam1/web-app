a0f3dc0da1d085a7ba27a8e87dea544045d13429
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-05-29 01:34:44 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,3 +6,21 @@ module.exports.isAuthenticated = (req, res, next) => {
     req.flash('error_msg', 'Please log in to view that resource');
     res.redirect('/login');
 };
+
+// Middleware function to check if the user is authenticated and is an employee
+module.exports.isEmployee = (req, res, next) => {
+    // Check if the user is authenticated
+    if (!req.isAuthenticated()) {
+        req.flash('error_msg', 'Please log in to view that resource');
+        return res.redirect('/login'); // Redirect to login if not authenticated
+    }
+
+    // Check if the user is an employee
+    if (!req.user.isEmployee) {
+        req.flash('error_msg', 'Access denied. Only employees are allowed.');
+        return res.status(403).send('Access denied. Only employees are allowed.'); // Return forbidden status if not an employee
+    }
+
+    // If authenticated and is an employee, proceed to the next middleware
+    next();
+};
